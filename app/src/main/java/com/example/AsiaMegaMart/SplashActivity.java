7a9aa3dca21af43675a3c64299f881a36eb9c647
shumssunam <shumssunam15@gmail.com>
2021-01-19 11:48:43 +0600

    Station 1

--- app/src/main/java/com/example/AsiaMegaMart/SplashActivity.java
+++ app/src/main/java/com/example/AsiaMegaMart/SplashActivity.java
@@ -2,41 +2,45 @@ package com.example.AsiaMegaMart;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView imageView;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
 
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_splash);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-    Thread splash = new Thread()
-    {
-        public void run()
-        {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView = findViewById(R.id.half_logo);
 
-            try {
-                sleep(2*700);
-                Intent intent = new Intent(com.example.AsiaMegaMart.SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        Thread splash = new Thread() {
+            public void run() {
+
+                try {
+                    sleep(2 * 700);
+                    Intent intent = new Intent(com.example.AsiaMegaMart.SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
 
-    };
-
-
-    splash.start();
-}
+        };
 
 
+        splash.start();
+
+
+        imageView.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in));
+    }
 
 
 }
